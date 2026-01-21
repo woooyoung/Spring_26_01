d7bdf2482cadf6badffb091da006077bc20f2ff0
woooyoung <axdsw121@gmail.com>
2026-01-21 16:35:58 +0900

    게시글 테스트 데이터 생성

--- src/main/java/com/example/demo/controller/UsrHomeController.java
+++ src/main/java/com/example/demo/controller/UsrHomeController.java
@@ -18,18 +18,33 @@ public class UsrHomeController {
 	public UsrHomeController() {
 		articles = new ArrayList<>();
 		lastArticleId = 0;
+
+		makeTestData();
+	}
+
+	private void makeTestData() {
+		for (int i = 1; i <= 10; i++) {
+			String title = "제목 " + i;
+			String body = "내용 " + i;
+
+			writeArticle(title, body);
+		}
+	}
+
+	private Article writeArticle(String title, String body) {
+		int id = lastArticleId + 1;
+
+		Article article = new Article(id, title, body);
+		articles.add(article);
+		lastArticleId++;
+
+		return article;
 	}
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
 	public Article doAdd(String title, String body) {
-
-		int id = lastArticleId + 1;
-
-		Article article = new Article(id, title, body);
-		articles.add(article);
-		lastArticleId++; 
-
+		Article article = writeArticle(title, body);
 		return article;
 	}
 

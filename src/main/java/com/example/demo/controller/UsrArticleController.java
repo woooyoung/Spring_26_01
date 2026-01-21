bd430954cebd42a418011a597e9dc549f60cc839
woooyoung <axdsw121@gmail.com>
2026-01-21 17:36:57 +0900

    게시글 수정, 상세보기 구현

--- src/main/java/com/example/demo/controller/UsrArticleController.java
+++ src/main/java/com/example/demo/controller/UsrArticleController.java
@@ -53,6 +53,36 @@ public class UsrArticleController {
 	}
 
 	// 액션메서드
+	@RequestMapping("/usr/article/getArticle")
+	@ResponseBody
+	public Object getArticle(int id) {
+
+		Article article = getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없음";
+		}
+
+		return article;
+	}
+
+	@RequestMapping("/usr/article/doModify")
+	@ResponseBody
+	public Object doModify(int id, String title, String body) {
+
+		Article article = getArticleById(id);
+
+		if (article == null) {
+			return id + "번 글은 없음";
+		}
+
+		article.setTitle(title);
+		article.setBody(body);
+
+//		return id + "번 글이 수정됨 " + article;
+		return article;
+	}
+
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
 	public String doDelete(int id) {

1fdec0365567295fce39748d6d521d45ab13665b
woooyoung <axdsw121@gmail.com>
2026-01-22 17:31:49 +0900

    로그인 아이디 중복체크

--- src/main/java/com/example/demo/service/MemberService.java
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -18,12 +18,24 @@ public class MemberService {
 	}
 
 	public int doJoin(String loginId, String loginPw, String name, String nickname, String cellphoneNum, String email) {
+		Member existsMember = getMemberByLoginId(loginId);
+
+		System.out.println("existsMember : " + existsMember);
+
+		if (existsMember != null) {
+			return -1;
+		}
+
 		memberRepository.doJoin(loginId, loginPw, name, nickname, cellphoneNum, email);
 		return memberRepository.getLastInsertId();
 	}
 
 	public Member getMemberById(int id) {
 		return memberRepository.getMemberById(id);
+	}
+
+	public Member getMemberByLoginId(String loginId) {
+		return memberRepository.getMemberByLoginId(loginId);
 	}
 
 }
